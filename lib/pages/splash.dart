97764303ee74257909238a5f5e5cb9a8e51d847a
Rizal Anggoro <gnoogler4@gmail.com>
2023-01-08 09:35:38 +0700

    update route

--- lib/pages/splash.dart
+++ lib/pages/splash.dart
@@ -1,6 +1,6 @@
 import 'dart:ui';
 
-import 'package:dicoding_restaurant_app/pages/restaurant_list.dart';
+import 'package:dicoding_restaurant_app/pages/home.dart';
 import 'package:dicoding_restaurant_app/utils/utils.dart';
 import 'package:flutter/material.dart';
 
@@ -13,55 +13,56 @@ class SplashPage extends StatelessWidget {
     Utils utils = Utils(context);
 
     return Scaffold(
-      body: Stack(children: [
-        Image.network(
-          'https://images.unsplash.com/photo-1578474846511-04ba529f0b88?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
-          height: double.infinity,
-          width: double.infinity,
-          fit: BoxFit.cover,
-        ),
-        SafeArea(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(
-                  left: 32,
-                  top: 32,
+      body: Stack(
+        children: [
+          Image.network(
+            'https://images.unsplash.com/photo-1578474846511-04ba529f0b88?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
+            height: double.infinity,
+            width: double.infinity,
+            fit: BoxFit.cover,
+          ),
+          SafeArea(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(
+                    left: 32,
+                    top: 32,
+                  ),
+                  child: Icon(Icons.restaurant_rounded,
+                      color: Colors.white.withOpacity(.64)),
                 ),
-                child: Icon(Icons.restaurant_rounded,
-                    color: Colors.white.withOpacity(.64)),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(
-                  left: 32,
-                  right: 32,
-                  top: 16,
-                ),
-                child: Text(
-                  'Less Talk,\nEat More',
-                  style: TextStyle(
-                    color: const Color(0xffd2a89b),
-                    fontSize: utils.textTheme.headline3!.fontSize,
-                    fontWeight: FontWeight.w500,
+                Padding(
+                  padding: const EdgeInsets.only(
+                    left: 32,
+                    right: 32,
+                    top: 16,
+                  ),
+                  child: Text(
+                    'Less Talk,\nEat More',
+                    style: TextStyle(
+                      color: const Color(0xffd2a89b),
+                      fontSize: utils.textTheme.headline3!.fontSize,
+                      fontWeight: FontWeight.w500,
+                    ),
                   ),
                 ),
-              ),
-              const Spacer(),
-              Container(
-                padding: const EdgeInsets.all(32),
-                margin: const EdgeInsets.all(16),
-                clipBehavior: Clip.hardEdge,
-                decoration: BoxDecoration(
-                  borderRadius: const BorderRadius.all(Radius.circular(16)),
-                  color: const Color(0xff3a2419).withOpacity(.56),
-                ),
-                child: BackdropFilter(
-                  filter: ImageFilter.blur(
-                    sigmaX: 8,
-                    sigmaY: 8,
+                const Spacer(),
+                Container(
+                  padding: const EdgeInsets.all(32),
+                  margin: const EdgeInsets.all(16),
+                  clipBehavior: Clip.hardEdge,
+                  decoration: BoxDecoration(
+                    borderRadius: const BorderRadius.all(Radius.circular(16)),
+                    color: const Color(0xff3a2419).withOpacity(.56),
                   ),
-                  child: Column(
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(
+                      sigmaX: 8,
+                      sigmaY: 8,
+                    ),
+                    child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Text(
@@ -87,19 +88,21 @@ class SplashPage extends StatelessWidget {
                             onPressed: () {
                               Navigator.pushReplacementNamed(
                                 context,
-                                RestaurantListPage.route,
+                                HomePage.route,
                               );
                             },
                             child: const Text('Get started'),
                           ),
                         ),
-                      ]),
-                ),
-              )
-            ],
-          ),
-        )
-      ]),
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
